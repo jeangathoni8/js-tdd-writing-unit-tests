eda877171f43c05a3bade88da439cb5290deb85f
jeangathoni8 <jeangathoni39@gmail.com>
2024-05-01 14:18:13 +0300

    final

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
-// Your tests here
+import { pointsForWord } from "../utils";
+import { describe, it, expect } from "@jest/globals";
+
+describe("pointsForWord", () => {
+    it("handles uppercase and lowercase input", () => {
+        const word = "tEsT";
+      
+        const points = pointsForWord(word);
+      
+        expect(points).toBe(7);
+      });
+});
